7c1574c6411617e634be8630149c2787834dbac3
Max Groot <19346100+MaxGroot@users.noreply.github.com>
2023-08-01 02:15:58 +0200

    Add a GcsWriter

--- flow/record/adapter/gcs.py
+++ flow/record/adapter/gcs.py
@@ -2,10 +2,10 @@ import logging
 from fnmatch import fnmatch
 from typing import Iterator, Union
 
-import google.cloud.storage.client as gcs_client
-import google.cloud.storage.fileio as gcs_io
+from google.cloud.storage.client import Client
+from google.cloud.storage.fileio import BlobReader, BlobWriter
 
-from flow.record.adapter import AbstractReader
+from flow.record.adapter import AbstractReader, AbstractWriter
 from flow.record.base import Record, RecordAdapter
 from flow.record.selector import CompiledSelector, Selector
 
@@ -16,6 +16,11 @@ Read usage: rdump gcs://[PROJECT-ID]:[BUCKET-ID]?path=[PATH]
 [PROJECT-ID]: Google Cloud Project ID
 [BUCKET-ID]: Bucket ID
 [path]: Path to look for files, with support for glob-pattern matching
+
+Write usage: rdump gcs://[PROJECT-ID]:[BUCKET-ID]?path=[PATH]
+[PROJECT-ID]: Google Cloud Project ID
+[BUCKET-ID]: Bucket ID
+[path]: Path to write records to
 """
 
 log = logging.getLogger(__name__)
@@ -29,7 +34,7 @@ class GcsReader(AbstractReader):
         project_name = uri[: uri.find(":")]
         bucket_name = uri[uri.find(":") + 1 :]
 
-        self.gcs = gcs_client.Client(project=project_name)
+        self.gcs = Client(project=project_name)
         self.bucket = self.gcs.bucket(bucket_name)
 
         # GCS Doesn't support iterating blobs using a glob pattern, so we have to do that ourselves.
@@ -53,7 +58,7 @@ class GcsReader(AbstractReader):
                 continue
             if self.glob and not fnmatch(blob.name, self.glob):
                 continue
-            blobreader = gcs_io.BlobReader(blob)
+            blobreader = BlobReader(blob)
 
             # Give the file-like object to RecordAdapter so it will select the right adapter by peeking into the
             # bytestream
@@ -63,3 +68,31 @@ class GcsReader(AbstractReader):
 
     def close(self) -> None:
         self.gcs.close()
+
+
+class GcsWriter(AbstractWriter):
+    def __init__(self, uri: str, path: str, **kwargs):
+        project_name = uri[: uri.find(":")]
+        bucket_name = uri[uri.find(":") + 1 :]
+        self.writer = None
+
+        self.gcs = Client(project=project_name)
+        self.bucket = self.gcs.bucket(bucket_name)
+
+        blob = self.bucket.blob(path)
+        self.writer = BlobWriter(blob, ignore_flush=True)
+        self.adapter = RecordAdapter(url=path, fileobj=self.writer, out=True, **kwargs)
+
+    def write(self, record: Record) -> None:
+        self.adapter.write(record)
+
+    def flush(self) -> None:
+        # https://cloud.google.com/python/docs/reference/storage/latest/google.cloud.storage.fileio.BlobWriter)
+        # Flushing without closing is not supported by the remote service and therefore calling it on this class
+        # normally results in io.UnsupportedOperation. However, that behavior is incompatible with some consumers and
+        # wrappers of fileobjects in Python.
+        pass
+
+    def close(self) -> None:
+        if self.writer:
+            self.writer.close()
